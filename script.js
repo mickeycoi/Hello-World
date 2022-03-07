9db6fa789357a170c9225dbce1774f1e89ea63c7
mickeycoi <datnguyenthanh1410@gmail.com>
2022-03-07 21:47:19 +0700

    feat: updated copy

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const addName = document.querySelector("#my-Name");
-const myName = document.createTextNode("  DAT");
+const myName = document.createTextNode("DAT DEP TRAI ^^");
 addName.appendChild(myName);
